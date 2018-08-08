cf2fd5f6663b3583d5ce36dd12e58eda53a5eb26
robin <robin.rb.luo@gmail.com>
2018-08-08 01:30:28 -0500

    Update flexibleVideos.js

--- scripts/flexibleVideos/flexibleVideos.js
+++ scripts/flexibleVideos/flexibleVideos.js
@@ -1,25 +1,25 @@
 function fixFrame(selector) {
-       zoom(selector);
+    zoom(selector);
        
-       var height = selector.find("iframe").height();
-       var wid = selector.find("iframe").width();
+    var height = selector.find("iframe").height();
+    var wid = selector.find("iframe").width();
         
-       if (height === wid) { 
-           if (wid < 540) {
-               height = 540; 
-               wid = 540; 
-           } 
-           height = 540;
-       }
+    if (height === wid) { 
+        if (wid < 540) {
+            height = 540; 
+            wid = 540; 
+        } 
+        height = 540;
+    }
         
-       var heightPercent = (height  / wid) * 100;
-       if (height > wid) { 
-           heightPercent = (wid / height) * 100; 
-       }
+    var heightPercent = (height  / wid) * 100;
+    if (height > wid) { 
+        heightPercent = (wid / height) * 100; 
+    }
         
-       selector.css({ position: "relative", width: "100%", height: "0", paddingBottom: heightPercent + "%" });
-       selector.find("iframe").css({ position: "absolute", width: "100%", height: "100%", left: "0", bottom: "0" });
-       selector.find(".tumblr_video_iframe").parent().css({ maxHeight: "540px" });
+    selector.css({ position: "relative", width: "100%", height: "0", paddingBottom: heightPercent + "%" });
+    selector.find("iframe").css({ position: "absolute", width: "100%", height: "100%", left: "0", bottom: "0" });
+    selector.find(".tumblr_video_iframe").parent(".video").css({ maxHeight: "540px" }); // back-up   
 }
 
 function flexibleFrames(selector) {
@@ -34,6 +34,9 @@ function zoom(selector) {
     var w = selector.find("iframe").width();
     var h = selector.find("iframe").height();
     var scale = $(".post").width() / w;
+    if (h > 540) {
+           h = 540;
+    }
     selector.find("iframe").css({ 
         width: scale * w,
         height: scale * h
