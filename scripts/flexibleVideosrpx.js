78500d17bfda2cd3ad5f407a305454d8cb3a242b
robin <robin.rb.luo@gmail.com>
2018-08-08 00:37:45 -0500

    Create flexibleVideosrpx.js

---
+++ scripts/flexibleVideosrpx.js
@@ -0,0 +1,44 @@
+function fixFrame(selector) {
+       zoom(selector);
+       
+       var height = selector.find("iframe").height();
+       var wid = selector.find("iframe").width();
+        
+       if (height === wid) { 
+           if (wid < 540) {
+               height = 540; 
+               wid = 540; 
+           } 
+           height = 540;
+       }
+       if (height > 540) { 
+           height = 540; 
+       }
+        
+       var heightPercent = (height  / wid) * 100;
+       if (height > wid) { 
+           heightPercent = (wid / height) * 100; 
+       }
+        
+       selector.css({ position: "relative", width: "100%", height: "0", paddingBottom: heightPercent + "%" });
+       selector.find("iframe").css({ position: "absolute", width: "100%", height: "100%", left: "0", bottom: "0" });
+       selector.find(".tumblr_video_iframe").parent().css({ maxHeight: "540px" });
+}
+
+function flexibleFrames(selector) {
+    selector.not(".resized").each(function() {
+       fixFrame($(this).find("iframe:not(.instagram-media, .bandcamp_audio_player, .spotify_audio_player, .soundcloud_audio_player)").parent());
+       $(this).addClass("resized");
+    });
+    $(".tumblr_video_container").css({ height: "auto", width: "auto" });
+}
+
+function zoom(selector) {
+    var w = selector.find("iframe").width();
+    var h = selector.find("iframe").height();
+    var scale = $(".post").width() / w;
+    selector.find("iframe").css({ 
+        width: scale * w,
+        height: scale * h
+    });  
+}
